c96111c502b377345f1effa6ce77c1f5ccdd4d11
Ashly Lau <ashlylau@gmail.com>
2020-02-29 16:49:21 +0000

    resolve merge conflict for backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,3 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
-
 const express = require('express')
 const app = express()
 const port = 3000
