65ca00fdaea47bb2cd33369aecaeb103d4955c54
Jonathan <jal18@ic.ac.uk>
2020-02-29 16:19:56 +0000

    Fixed the screen switch

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { View, Button, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
